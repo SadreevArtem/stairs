59beab0978ab0cb640d0118a295d331e0d782624
Sadreev Artem <sadreev@echo-company.ru>
2024-05-22 22:48:00 +0500

    add yandex metrics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,30 +6,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head></Head>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-      
-                ym(97357984, "init", {
-                      clickmap:true,
-                      trackLinks:true,
-                      accurateTrackBounce:true
-                });
-              `,
-        }}
-      />
-      <noscript>
-        <div>
-          <img
-            src="https://mc.yandex.ru/watch/97357984"
-            style={{ position: "absolute", left: "-9999px" }}
-            alt=""
-          />
-        </div>
-      </noscript>
       <body className="min-h-screen">
         <Main />
         <NextScript />
